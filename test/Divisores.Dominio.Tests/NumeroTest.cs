f37c94b912e8c870bfba298b79790116fd54233c
h1fernando <hd1.fernandosouza@gmail.com>
2021-02-06 20:10:42 -0300

    tests: adição de testes de divisores para números 2 e 1

--- test/Divisores.Dominio.Tests/NumeroTest.cs
+++ test/Divisores.Dominio.Tests/NumeroTest.cs
@@ -7,14 +7,34 @@ namespace Divisores.Dominio.Tests
 {
     public class NumeroTest
     {
-        [Fact(DisplayName = "Deve retornar divisores.")]
-        public void DeveRetonarDivisores()
+        [Fact(DisplayName = "Deve retornar divisores de 45.")]
+        public void DeveRetonarDivisoresDeQuarentaECinco()
         {
             Numero numero = new(45);
 
             var divisores = numero.Divisores();
 
-            divisores.Should().Equal(new List<int> { 1, 3, 5, 9, 45 });
+            divisores.Should().Equal(new HashSet<int> { 1, 3, 5, 9, 15, 45 });
+        }
+
+        [Fact(DisplayName = "Deve retornar divisores de dois.")]
+        public void DeveRetonarDivisoresDeDois()
+        {
+            Numero numero = new(2);
+
+            var divisores = numero.Divisores();
+
+            divisores.Should().Equal(new HashSet<int> { 1, 2 });
+        }
+
+        [Fact(DisplayName = "Deve retornar divisores de um.")]
+        public void DeveRetonarDivisoresUm()
+        {
+            Numero numero = new(1);
+
+            var divisores = numero.Divisores();
+
+            divisores.Should().Equal(new HashSet<int> { 1 });
         }
     }
 }
