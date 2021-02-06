09a38215e13946b89a3f86dce342e8c2764cd349
h1fernando <hd1.fernandosouza@gmail.com>
2021-02-06 19:36:27 -0300

    feat: criação do tipo Numero Com erros para não passar no primeiro teste

---
+++ src/Divisore.Dominio/Numero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Divisore.Dominio
+{
+    public class Numero
+    {
+        private readonly int _numero;
+
+        public Numero(int numero)
+        {
+            _numero = numero;
+        }
+
+        public IList<int> Divisores()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
