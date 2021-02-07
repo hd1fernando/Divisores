eb1b66f8f15ef2c144bd403ef1c35c1294f651bb
h1fernando <hd1.fernandosouza@gmail.com>
2021-02-07 09:28:40 -0300

    feat: criação tipo DivisoresResult

---
+++ src/Divisore.Dominio/DivisoresResult.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Divisores.Dominio
+{
+    public class DivisoresResult
+    {
+        public Numero NumeroDeEntrada { get; }
+        public IEnumerable<long> NumerosDivisores { get; }
+        public IEnumerable<Numero> DivisoresPrimos { get; }
+
+        public DivisoresResult(
+            Numero numeroDeEntrada,
+            IEnumerable<long> numerosDivisores,
+            IEnumerable<Numero> divisoresPrimos)
+        {
+            NumeroDeEntrada = numeroDeEntrada;
+            NumerosDivisores = numerosDivisores;
+            DivisoresPrimos = divisoresPrimos;
+        }
+    }
+}
