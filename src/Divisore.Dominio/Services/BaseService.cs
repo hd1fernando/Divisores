fc608569e30868e22552ede09f62d7f6ec50344e
h1fernando <hd1.fernandosouza@gmail.com>
2021-02-07 08:58:29 -0300

    feat: incio criação DecomposicaoService

---
+++ src/Divisore.Dominio/Services/BaseService.cs
@@ -0,0 +1,18 @@
+﻿using Divisore.Dominio.Interfaces;
+using Divisore.Dominio.Notificacoes;
+
+namespace Divisore.Dominio.Services
+{
+    public class BaseService
+    {
+        private INotificador _notificador;
+
+        protected BaseService(INotificador notificador)
+        {
+            _notificador = notificador;
+        }
+
+        protected virtual void Notificar(string mensagem)
+            => _notificador.Manipular(new Notificacao(mensagem));
+    }
+}
---
+++ src/Divisore.Dominio/Services/DecomposicaoService.cs
@@ -0,0 +1,29 @@
+﻿using Divisore.Dominio.Interfaces;
+using System.Collections.Generic;
+
+namespace Divisore.Dominio.Services
+{
+    public class DecomposicaoService : BaseService, IDecomposicaoService
+    {
+        public DecomposicaoService(INotificador notificador) : base(notificador)
+        { }
+
+        public DivisoresResult EncontarDivisores(Numero numero)
+        {
+            HashSet<Numero> primos = new();
+            var divisores = numero.Divisores();
+
+            foreach (var divisor in divisores)
+            {
+                Numero n = divisor;
+                if (n.EhPrimo())
+                    primos.Add(n);
+            }
+
+            return new DivisoresResult(
+                     numero,
+                     new HashSet<Numero> { 1, 3, 5, 9, 15, 45 },
+                     primos);
+        }
+    }
+}
